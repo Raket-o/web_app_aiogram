15a5b28f68ac6ccc23a1a52f52467f41c5aa4a27
Mikhail Sobolev <kseon@inbox.ru>
2024-11-17 22:09:01 +0400

    some_changes

--- aiogram_bot/config_data/config.py
+++ aiogram_bot/config_data/config.py
@@ -10,12 +10,6 @@ if not find_dotenv():
 else:
     load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
-MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
-
-LOCAL_UTC = os.getenv("LOCAL_UTC")
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 START_MESSAGE = os.getenv("START_MESSAGE")
 ULR_WEB_APP = os.getenv("ULR_WEB_APP")
-DEFAULT_MINING_IN_MINUTE = os.getenv("DEFAULT_MINING_IN_MINUTE")
